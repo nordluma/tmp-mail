925bf5ad8b882e76b48aa8a7faa944e388bc87aa
nordluma <maxnordlund.95@hotmail.com>
2023-09-05 20:32:54 +0300

    feat: define mail structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct Mail {
+    pub from: String,
+    pub to: Vec<String>,
+    pub data: String,
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
